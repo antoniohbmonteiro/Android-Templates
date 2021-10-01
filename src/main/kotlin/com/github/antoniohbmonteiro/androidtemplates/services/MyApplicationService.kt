e87e03e593e3a101a7bc59c7d732cd13fd4a0d55
GitHub Action <action@github.com>
2021-10-01 20:12:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/antoniohbmonteiro/androidtemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.antoniohbmonteiro.androidtemplates.services
+
+import com.github.antoniohbmonteiro.androidtemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
